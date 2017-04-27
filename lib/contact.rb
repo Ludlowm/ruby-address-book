6c90191cdf19513dd19d7a1a87eb4530dc5971b6
Aubrey Masten and Marko Ludlow <marko@>
2017-04-27 10:38:09 -0700

    add methods for email, phone, address and contact objects

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,4 +1,16 @@
 class Contact
+  @@contacts = []
+  attr_reader(:first_name, :last_name, :job_title,:company,:mailing_address_list,:phone_num_list,:email_list)
+
+  def initialize (parameters)
+    @first_name = parameters.fetch(:first_name)
+    @last_name = parameters.fetch(:last_name)
+    @job_title = parameters.fetch(:job_title)
+    @company = parameters.fetch(:company)
+    @mailing_address_list = parameters.fetch(:mailing_address_list)
+    @phone_num_list = parameters.fetch(:phone_num_list)
+    @email_list = parameters.fetch(:email_list)
+  end
 
 end
 
@@ -12,9 +24,20 @@ class Phone
 end
 
 class Email
-
+  attr_reader(:address, :type)
+  def initialize (parameters)
+    @address = parameters.fetch(:address)
+    @type = parameters.fetch(:type)
+  end
 end
 
 class Address
-
+  attr_reader(:street, :state, :type, :city, :zip)
+  def initialize (parameters)
+    @street = parameters.fetch(:street)
+    @state = parameters.fetch(:state)
+    @type = parameters.fetch(:type)
+    @city = parameters.fetch(:city)
+    @zip = parameters.fetch(:zip)
+  end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -14,14 +14,63 @@ describe(Phone) do
     end
   end
 end
-# describe(Email) do
-#   describe('initialize') do
-#     it('creates a new email address') do
-#       test_email = Email.new({
-#         :address => "testemail@gmail.com",
-#         :type => "work"
-#       })
-#       expect(test_email.address).to(eq("testemail@gmail.com"))
-#     end
-#   end
-# end
+
+describe(Email) do
+  describe('initialize') do
+    it('creates a new email address') do
+      test_email = Email.new({
+        :address => "bobhendley@gmail.com",
+        :type => "work"
+      })
+      expect(test_email.address).to(eq("bobhendley@gmail.com"))
+    end
+  end
+end
+
+describe(Address) do
+  describe('initialize') do
+    it('creates a new address') do
+      test_address = Address.new({
+        :street => "5679 Main Drive",
+        :state => "MI",
+        :type => "work",
+        :city => "Kansas City",
+        :zip => "87201"
+      })
+      expect(test_address.street).to(eq("5679 Main Drive"))
+    end
+  end
+end
+
+describe(Contact) do
+  describe('initialize') do
+    it('creates a new contact') do
+      test_phone = Phone.new({
+        :area_code => "503",
+        :number => "5038675309",
+        :type => "personal"
+      })
+      test_email = Email.new({
+        :address => "bobhendley@gmail.com",
+        :type => "work"
+      })
+      test_address = Address.new({
+        :street => "5679 Main Drive",
+        :state => "MI",
+        :type => "work",
+        :city => "Kansas City",
+        :zip => "87201"
+      })
+      test_contact = Contact.new({
+        :first_name => "Sylvester",
+        :last_name => "James",
+        :job_title => "Mayor",
+        :company => "Kansas City",
+        :mailing_address_list => [test_address],
+        :phone_num_list => [test_phone],
+        :email_list => [test_email]
+      })
+      expect(test_contact.first_name).to(eq("Sylvester"))
+    end
+  end
+end
