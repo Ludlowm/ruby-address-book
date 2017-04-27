8b9214849f7c862d8cc483c724a89eb76a6ee33c
Aubrey Masten and Marko Ludlow <marko@>
2017-04-27 15:28:44 -0700

    refactor update coded

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require 'pry'
 also_reload('lib/**/*.rb')
 
 get('/') do
-  binding.pry
   erb(:index)
 end
 
@@ -20,23 +19,37 @@ get('/inspect/:id') do
   erb(:index)
 end
 
-post('/update_ph') do
-  binding.pry
-  Contact.all[params.fetch("id").to_i].add_phone(params)
+post('/update') do
   @contact = Contact.all[params.fetch("id").to_i]
+  binding.pry
+  case params.fetch("add")
+  when "addr"
+    Contact.all[params.fetch("id").to_i].add_address(params)
+  when "em"
+    Contact.all[params.fetch("id").to_i].add_email(params)
+  when "ph"
+    Contact.all[params.fetch("id").to_i].add_phone(params)
+  end
   erb(:index)
 end
 
-post('/update_em') do
-  binding.pry
-  Contact.all[params.fetch("id").to_i].add_email(params)
-  @contact = Contact.all[params.fetch("id").to_i]
-  erb(:index)
-end
-
-post('/update_addr') do
-  binding.pry
-  Contact.all[params.fetch("id").to_i].add_address(params)
-  @contact = Contact.all[params.fetch("id").to_i]
-  erb(:index)
-end
+# post('/update_ph') do
+#   binding.pry
+#   Contact.all[params.fetch("id").to_i].add_phone(params)
+#   @contact = Contact.all[params.fetch("id").to_i]
+#   erb(:index)
+# end
+#
+# post('/update_em') do
+#   binding.pry
+#   Contact.all[params.fetch("id").to_i].add_email(params)
+#   @contact = Contact.all[params.fetch("id").to_i]
+#   erb(:index)
+# end
+#
+# post('/update_addr') do
+#   binding.pry
+#   Contact.all[params.fetch("id").to_i].add_address(params)
+#   @contact = Contact.all[params.fetch("id").to_i]
+#   erb(:index)
+# end
